18871061f577ff838dd84e9bb34f5b1e303cba69
Roninthevictory <158804933+Roninthevictory@users.noreply.github.com>
2025-11-02 18:36:56 -0600

    Update metadata.js

--- api/metadata.js
+++ api/metadata.js
@@ -1,1 +1,13 @@
+export default async function handler(req, res) {
+  return res.status(200).json({
+    metadata: [
+      {
+        key: "verified",
+        name: "RVO Verified",
+        description: "Confirmed through the RVO INC Gateway",
+        type: 5
+      }
+    ]
+  });
+}
 
